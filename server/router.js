93d176c9983d834bdfa0e3d1dfdc25d241f3a04f
Daniel Greaves <dgreaves18@gmail.com>
2023-02-21 15:57:30 -0800

    Added endpoints

--- server/router.js
+++ server/router.js
@@ -5,6 +5,30 @@ router.get('/', (req, res) => {
   res.status(200).send('Howdy');
 })
 
-router.get('/qa/questions', controller.getQuestionsByProductId);
+router.get('/qa/questions', controller.getAllQuestionsByProductId);
+
+router.get('/qa/questions/:question_id/answers', controller.getAnswersByQuestionId);
+
+router.post('/qa/questions/', controller.addQuestionById);
+
+router.post('/qa/questions/:question_id/answers', controller.addAnswerById);
+
+router.put('/qa/questions/:question_id/helpful', controller.updateQuestionHelpful);
+
+router.put('/qa/questions/:question_id/report', controller.handleQuestionReport);
+
+router.put('/qa/answers/:answer_id/helpful', controller.updateAnswerHelpful);
+
+router.put('/qa/answers/:answer_id/report', controller.handleAnswerReport);
+
+// Routes from FEC Mecca
+// router.get('/qa/questions', controllers.questions.getAllQuestionsByProductId);
+// router.get('/qa/questions/:question_id/answers', controllers.questions.getAnswersByQuestionId);
+// router.post('/qa/questions/', controllers.questions.addQuestionById);
+// router.post('/qa/questions/:question_id/answers', controllers.questions.addAnswerById);
+// router.put('/qa/questions/:question_id/helpful', controllers.questions.updateQuestionHelpful);
+// router.put('/qa/questions/:question_id/report', controllers.questions.handleQuestionReport);
+// router.put('/qa/answers/:answer_id/helpful', controllers.questions.updateAnswerHelpful);
+// router.put('/qa/answers/:answer_id/report', controllers.questions.handleAnswerReport);
 
 module.exports = router;
